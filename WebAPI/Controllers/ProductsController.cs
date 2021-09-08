d89e04f994a0ea4df324f16a34d58f21be8745b5
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2021-09-08 16:35:36 +0300

    08.09.2021

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Business.Concretes;
+using Core.Utilities.Results;
+using DataAccess.Concretes.EntityFramework;
+using Entities.Concretes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IDataResult<List<Product>> Get()
+        {
+            var result= _productService.GetAll();
+            return result; 
+        }
+    }
+}
